15c33fa517bb6fe2d67846fe798cd5558a547755
amit kumar maurya <amitmauryass163@gmail.com>
2025-03-02 17:37:48 +0530

    refactor name

--- user-service/src/main/java/com/banking/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/banking/userservice/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.banking.userservice.user_service;
+package com.banking.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
