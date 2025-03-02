9e8d84a573d19d7257d244eab2ea4b98e01eee77
amit kumar maurya <amitmauryass163@gmail.com>
2025-03-02 19:51:49 +0530

    user entity and database connections done

---
+++ user-service/src/main/java/com/banking/userservice/entity/Users.java
@@ -0,0 +1,42 @@
+package com.banking.userservice.entity;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "userDetails")
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long userId;
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private String password;
+	private Date dob;
+	private String address;
+	
+	@Enumerated(EnumType.STRING) 
+	private KYCStatus kycStatus;
+
+	    public enum KYCStatus {
+	        PENDING,
+	        VERIFIED,
+	        REJECTED
+	    }
+	    
+	private Timestamp createdAt;
+}
