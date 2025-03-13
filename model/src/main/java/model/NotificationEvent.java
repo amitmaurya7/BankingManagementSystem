06cc70b6bb6a52a0a875a8c173bab02293dd9bf7
amit kumar maurya <amitmauryass163@gmail.com>
2025-03-13 13:00:08 +0530

    create common module for dto

---
+++ model/src/main/java/model/NotificationEvent.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationEvent {
+
+	private Long userId;
+	private String message;
+
+	@Enumerated(EnumType.STRING)
+	private NotificationType type;
+
+	public enum NotificationType {
+		EMAIL, SMS
+	}
+}
